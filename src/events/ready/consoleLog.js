e20c0fba15c780aec95b973a4af8a438cc28c8b1
Toabdjzl <84243201+Toabdjzl@users.noreply.github.com>
2024-12-19 22:19:14 +0100

    Update consoleLog.js

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,3 +1,7 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = (client) => {
     console.log(`🦊 ${client.user.username} is ready!`);
+
+    client.user.setActivity({name: 'Beep. Boop.', type: ActivityType.Custom})
 };
