b3b71141ae31ce3fd69f8e67ec1bfad99ec2c76d
Toabdjzl <witttobi@outlook.de>
2026-01-12 11:33:50 +0100

    🚚 Renamed events to match common js naming

---
+++ src/events/guildMemberAdd/sendWelcomeMessage.js
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @param {import('discord.js').GuildMember} guildMember 
+ */
+
+module.exports = async (guildMember) => {
+    try {
+        
+    } catch (error) {
+        console.log(`⚠️  ${__filename}:\n${error}`);
+    }
+}
