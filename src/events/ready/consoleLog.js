31bd4df6bb07c0994ba29fcec6cfbace25c3fd0f
Toabdjzl <witttobi@outlook.de>
2026-01-12 11:34:55 +0100

    👽️ Renamed ready event to clientReady

--- src/events/ready/consoleLog.js
+++
@@ -1,7 +0,0 @@
-const { ActivityType } = require('discord.js');
-
-module.exports = (client) => {
-    console.log(`🦊 ${client.user.username} is ready!`);
-
-    client.user.setActivity({name: 'Beep. Boop.', type: ActivityType.Custom})
-};
